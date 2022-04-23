28e17ea1837eeacf0a63722e45acb258496e7ab2
Grace <huang.yahui.grace@gmail.com>
2022-04-24 02:29:26 +0800

    Android & JavaScript雙向呼叫

---
+++ app/src/main/java/com/myyhhuang/mywebviewapp/ui/notifications/MyWebViewClient.java
@@ -0,0 +1,12 @@
+package com.myyhhuang.mywebviewapp.ui.notifications;
+
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.BridgeWebViewClient;
+
+public class MyWebViewClient extends BridgeWebViewClient {
+
+    public MyWebViewClient(BridgeWebView webView) {
+        super(webView);
+    }
+
+}
